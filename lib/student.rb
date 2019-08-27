e877305b9c8c23a6100a1defbc3dd43e7298278a
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-27 04:21:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,17 @@ class Student
     @id = id 
     @name = name 
     @grade = grade 
-    
   end 
+  
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
   
 end
