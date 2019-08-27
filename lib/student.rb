a0d3dc5e000c847f98e2a65a44c1879a5811960b
Christian Polanco <chrispolanco@sbcglobal.net>
2019-08-27 04:31:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,12 @@ class Student
  
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
- 
+  end 
+  
+  def self.create(name, grade)
+    song = Student.new(name, grade)
+    song.save
+    song
   end
   
   
